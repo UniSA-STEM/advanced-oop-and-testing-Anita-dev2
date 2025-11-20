5b1068df6e059856a1dfd61e0d46a981a61483fc
Anita-dev2 <maray160@mymail.unisa.edu.au>
2025-11-20 23:32:57 +1100

    - Adding and amending tests

--- program.py
+++ program.py
@@ -13,10 +13,17 @@ from animal import Bird
 from animal import Reptile
 from animal import Fish
 from enclosure import Bushland
+from enclosure import Farmyard
+from enclosure import Vivarium
+from enclosure import Aviary
+from enclosure import Aquatic
+from enclosure import Savannah
 from staff import Zookeeper
 from staff import Veterinarian
 
 class TestZoo:
+    # Setting up with Fixtures
+
     @pytest.fixture
     def mammal(self):
         return Mammal("Caramello", "koala", 5, "herbivore")
@@ -33,11 +40,29 @@ class TestZoo:
     def reptile(self):
         return Reptile("Rango", "lizard", 14, "carnivore")
 
-
-
     @pytest.fixture
     def bushland(self):
         return Bushland()
+
+    @pytest.fixture
+    def farmyard(self):
+        return Farmyard()
+
+    @pytest.fixture
+    def vivarium(self):
+        return Vivarium()
+
+    @pytest.fixture
+    def aviary(self):
+        return Aviary()
+
+    @pytest.fixture
+    def aquatic(self):
+        return Aquatic()
+
+    @pytest.fixture
+    def savannah(self):
+        return Savannah()
 
     @pytest.fixture
     def zookeeper(self):
@@ -54,6 +79,28 @@ class TestZoo:
         assert bird.animal_name == "Tweety"
         assert fish.animal_name == "Nemo"
         assert reptile.animal_name == "Rango"
+
+    def test_name_type(self, mammal, bird, fish, reptile):
+        assert isinstance(mammal.animal_name, str)
+        assert isinstance(bird.animal_name, str)
+        assert isinstance(fish.animal_name, str)
+        assert isinstance(reptile.animal_name, str)
+
+    def test_invalid_name(self, mammal):
+        with pytest.raises(TypeError):
+            Mammal(123, "lion", 5, "carnivore")
+
+    def test_invalid_age(self, reptile):
+        with pytest.raises(TypeError):
+            Reptile("Rango", "lizard", "5", "carnivore")
+
+    def test_invalid_species(self, fish):
+        with pytest.raises(TypeError):
+            Fish("Nemo", 3, 4, "omnivore")
+
+    def test_invalid_dietary_needs(self, bird):
+        with pytest.raises(ValueError):
+            Bird("Tweety", "bird", 5, "vegetarian")
 
     def test_get_age(self, mammal, bird, fish, reptile):
         assert mammal.age == 5
@@ -99,18 +146,43 @@ class TestZoo:
 
     # Tests for Enclosure Class
 
-    def test_get_capacity(self, bushland):
+    def test_get_capacity(self, bushland, aviary, vivarium, farmyard, savannah, aquatic):
         assert bushland.capacity == 6
+        assert aviary.capacity == 6
+        assert vivarium.capacity == 6
+        assert farmyard.capacity == 6
+        assert savannah.capacity == 6
+        assert aquatic.capacity == 6
 
-    def test_can_accept(self, bushland, mammal):
+    def test_can_accept(self, bushland, aviary, vivarium, farmyard, savannah, aquatic, mammal):
         assert bushland.can_accept(mammal) == True
+        assert aviary.can_accept(mammal) == False
+        assert vivarium.can_accept(mammal) == False
+        assert farmyard.can_accept(mammal) == False
+        assert savannah.can_accept(mammal) == False
+        assert aquatic.can_accept(mammal) == False
 
-    def test_get_fed(self, bushland):
+    def test_get_fed(self, bushland, aviary, vivarium, farmyard, savannah, aquatic):
         assert bushland.fed == False
+        assert aviary.fed == False
+        assert vivarium.fed == False
+        assert farmyard.fed == False
+        assert savannah.fed == False
+        assert aquatic.fed == False
 
-    def test_get_clean(self, bushland):
+    def test_get_clean(self, bushland, aviary, vivarium, farmyard, savannah, aquatic):
         assert bushland.clean == False
+        assert aviary.clean == False
+        assert vivarium.clean == False
+        assert farmyard.clean == False
+        assert savannah.clean == False
+        assert aquatic.clean == False
 
-    def test_get_health_check(self, bushland):
+    def test_get_health_check(self, bushland, aviary, vivarium, farmyard, savannah, aquatic):
         assert bushland.health_check == False
+        assert aviary.health_check == False
+        assert vivarium.health_check == False
+        assert farmyard.health_check == False
+        assert savannah.health_check == False
+        assert aquatic.health_check == False
 
