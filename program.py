19fe695517438db5d01c2a4166d84e59cda7f21b
Anita-dev2 <maray160@mymail.unisa.edu.au>
2025-11-20 23:04:35 +1100

    - Adding and amending tests

--- program.py
+++ program.py
@@ -9,42 +9,108 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 import pytest
 from animal import Mammal
+from animal import Bird
+from animal import Reptile
+from animal import Fish
 from enclosure import Bushland
 from staff import Zookeeper
+from staff import Veterinarian
 
-@pytest.fixture
-def animal_fixture():
-    return Mammal("Caramello", "koala", 5, "herbivore")
+class TestZoo:
+    @pytest.fixture
+    def mammal(self):
+        return Mammal("Caramello", "koala", 5, "herbivore")
 
-@pytest.fixture
-def enclosure_fixture():
-    return Bushland()
+    @pytest.fixture
+    def bird(self):
+        return Bird("Tweety", "bird", 7, "omnivore")
 
-@pytest.fixture
-def staff_fixture():
-    return Zookeeper("Bill")
+    @pytest.fixture
+    def fish(self):
+        return Fish("Nemo", "fish", 9, "omnivore")
 
-def test_get_name(animal_fixture):
-    assert animal_fixture.animal_name == "Caramello"
+    @pytest.fixture
+    def reptile(self):
+        return Reptile("Rango", "lizard", 14, "carnivore")
 
-def test_get_age(animal_fixture):
-    assert animal_fixture.age == 5
 
-def test_get_dietary_needs(animal_fixture):
-    assert animal_fixture.diet == "Herbivore"
 
-def test_get_species(animal_fixture):
-    assert animal_fixture.species == "koala"
+    @pytest.fixture
+    def bushland(self):
+        return Bushland()
 
-def test_get_health(animal_fixture):
-    assert animal_fixture.get_health() == "Healthy"
+    @pytest.fixture
+    def zookeeper(self):
+        return Zookeeper("Bill")
 
-def test_health_status(animal_fixture):
-    assert animal_fixture.health_status() == "Healthy" or "Injured" or "Sick" or "Behaviourally Challenged"
+    @pytest.fixture
+    def vet(self):
+        return Veterinarian("Ben")
 
-def test_str(animal_fixture):
-    assert animal_fixture.__str__() == "Meet Caramello! Caramello is a koala, and is 5 years old. Caramello is a Herbivore.\nTo learn more about Caramello, ask one of our friendly Zookeepers!\n"
+    # Tests for Animal Class
 
-def test_get_capacity(enclosure_fixture):
-    assert enclosure_fixture.capacity == 6
+    def test_get_name(self, mammal, bird, fish, reptile):
+        assert mammal.animal_name == "Caramello"
+        assert bird.animal_name == "Tweety"
+        assert fish.animal_name == "Nemo"
+        assert reptile.animal_name == "Rango"
+
+    def test_get_age(self, mammal, bird, fish, reptile):
+        assert mammal.age == 5
+        assert mammal.age != "5"
+        assert bird.age == 7
+        assert bird.age != "7"
+        assert fish.age == 9
+        assert fish.age != "9"
+        assert reptile.age == 14
+        assert reptile.age != "14"
+
+    def test_get_dietary_needs(self, mammal, bird, fish, reptile):
+        assert mammal.diet == "Herbivore"
+        assert bird.diet == "Omnivore"
+        assert fish.diet == "Omnivore"
+        assert reptile.diet == "Carnivore"
+
+    def test_get_species(self, mammal, bird, fish, reptile):
+        assert mammal.species == "koala"
+        assert bird.species == "bird"
+        assert fish.species == "fish"
+        assert reptile.species == "lizard"
+
+    def test_get_health(self, mammal, bird, fish, reptile):
+        assert mammal.get_health() == "Healthy"
+        assert bird.get_health() == "Healthy"
+        assert fish.get_health() == "Healthy"
+        assert reptile.get_health() == "Healthy"
+
+    def test_health_status(self, mammal, bird, fish, reptile):
+        assert mammal.health_status() == "Healthy" or "Injured" or "Sick" or "Behaviourally Challenged"
+        assert bird.health_status() == "Healthy" or "Injured" or "Sick" or "Behaviourally Challenged"
+        assert fish.health_status() == "Healthy" or "Injured" or "Sick" or "Behaviourally Challenged"
+        assert reptile.health_status() == "Healthy" or "Injured" or "Sick" or "Behaviourally Challenged"
+
+
+    def test_str(self, mammal, bird, fish, reptile):
+        assert mammal.__str__() == "Meet Caramello! Caramello is a koala, and is 5 years old. Caramello is a Herbivore.\nTo learn more about Caramello, ask one of our friendly Zookeepers!\n"
+        assert bird.__str__() == "Meet Tweety! Tweety is a bird, and is 7 years old. Tweety is a Omnivore.\nTo learn more about Tweety, ask one of our friendly Zookeepers!\n"
+        assert fish.__str__() == "Meet Nemo! Nemo is a fish, and is 9 years old. Nemo is a Omnivore.\nTo learn more about Nemo, ask one of our friendly Zookeepers!\n"
+        assert reptile.__str__() == "Meet Rango! Rango is a lizard, and is 14 years old. Rango is a Carnivore.\nTo learn more about Rango, ask one of our friendly Zookeepers!\n"
+
+
+    # Tests for Enclosure Class
+
+    def test_get_capacity(self, bushland):
+        assert bushland.capacity == 6
+
+    def test_can_accept(self, bushland, mammal):
+        assert bushland.can_accept(mammal) == True
+
+    def test_get_fed(self, bushland):
+        assert bushland.fed == False
+
+    def test_get_clean(self, bushland):
+        assert bushland.clean == False
+
+    def test_get_health_check(self, bushland):
+        assert bushland.health_check == False
 
