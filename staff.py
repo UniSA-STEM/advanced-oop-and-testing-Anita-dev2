41e108148917d838dd3810c96855cc184420636f
Anita-dev2 <maray160@mymail.unisa.edu.au>
2025-11-21 23:59:46 +1100

    - Finalised methods - Included docstrings and comments

--- staff.py
+++ staff.py
@@ -1,14 +1,18 @@
 """
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: staff.py
+Description: This module includes the Staff class and its children classes
+Author: Anita Maratheftis
+ID: 110467133
+Username: maray160
 This is my own work as defined by the University's Academic Integrity Policy.
 """
 from abc import ABC, abstractmethod
 
 class Staff(ABC):
+    """
+    This is the abstract class for staff
+    """
+    # Set staff id to 1
     __next_id = 1
 
     def __init__(self, name):
@@ -17,67 +21,94 @@ class Staff(ABC):
         self.__clocked_in = False
         Staff.__next_id = Staff.__next_id + 1
 
-
     def get_staff_name(self):
+        """
+        This is a getter for staff name
+        :return: self.__name: str
+        """
         return self.__name
 
     staff_name = property(get_staff_name)
 
+
     def get_staff_id(self):
+        """
+        This is a getter for staff id
+        :return: self.__id: int
+        """
         return self.__id
 
+    # Set property for staff id
     staff_id = property(get_staff_id)
 
     def get_attendance(self):
+        """
+        This is a getter for attendance
+        :return: self.__clocked_in: boolean
+        """
         return self.__clocked_in
 
+    # Set property for attendance
     working = property(get_attendance)
 
     def clock_in(self):
+        """
+        This function allows a staff member to clock in for work, or validates that they are clocked in if they try twice
+        :return:
+        """
+        # If self.__clocked_in returns False, reset attribute to True
         if not self.working:
             self.__clocked_in = True
-            print(f"{self.staff_name} has clocked in for work.")
+            # Display result to user
+            print(f"{self.staff_name} has clocked in for work.\n")
         else:
-            print(f"{self.staff_name} is already at work!")
+            # Display to user that the staff member is already at work (self.__clocked_in returned True)
+            print(f"{self.staff_name} is already at work!\n")
 
     def clock_out(self):
+        """
+        This function allows a staff member to clock out for work, or validates that they are clocked out if they try twice
+        :return:
+        """
+        # if self.__clocked_in is True, reset attribute to False
         if self.working:
             self.__clocked_in = False
-            print(f"{self.staff_name} has successfully clocked out for today.")
+            # Display result to user
+            print(f"{self.staff_name} has successfully clocked out for today.\n")
         else:
-            print(f"{self.staff_name} has already finished for today.")
+            # Display to user that the staff member has already clocked out (self.__clocked_in returned False)
+            print(f"{self.staff_name} has already finished for today.\n")
 
+    # Abstract string method
     @abstractmethod
     def __str__(self):
         pass
 
-
 class Zookeeper(Staff):
+    """
+    This is the child class of staff, zookeeper
+    """
     def __init__(self, name):
         super().__init__(name)
 
     def __str__(self):
+        """
+        This is the string method for zookeeper
+        :return: str
+        """
         return f"Name: {self.staff_name}\nStaff ID: {self.staff_id}\nRole: Zookeeper\nAllocated Tasks: Feeding + Cleaning\n"
 
 
 class Veterinarian(Staff):
+    """
+    This is the child class of staff, veterinarian
+    """
     def __init__(self, name):
         super().__init__(name)
 
     def __str__(self):
+        """
+        This is the string method for veterinarian
+        :return:
+        """
         return f"Name: {self.staff_name}\nStaff ID: {self.staff_id}\nRole: Veterinarian\nAllocated Tasks: Health Checks\n"
-
-
-
-staff1 = Zookeeper("Bob")
-staff1.clock_in()
-staff1.get_staff_id()
-print(staff1)
-zoo1 = Zookeeper("Sally")
-print(zoo1)
-zoo1.clock_in()
-zoo1.clock_in()
-zoo1.clock_out()
-vet1 = Veterinarian("Rob")
-vet1.clock_in()
-print(vet1)
